8853cd3c9befe9bbbfe52fc3a55f233dbb108867
nidzgen2 <163583322+nidzgen2@users.noreply.github.com>
2024-05-27 11:58:03 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ import {
   sepolia,
 } from "viem/chains";
 import { createConfig } from "wagmi";
-import { erc20Abi, optimismPortalAbi } from "./abi";
+import { erc20Abi, optimismPortalAbi,  } from "./abi";
 
 import type { PublicL1ClientWithChain, PublicL2ClientWithChain } from "./types";
 import { chainConfig, publicActionsL1, publicActionsL2 } from "viem/op-stack";
@@ -29,6 +29,31 @@ import { chainConfig, publicActionsL1, publicActionsL2 } from "viem/op-stack";
  * TODO:
  * - load walletConnect project id from the environment:
  */
+const JibchainTestnet = defineChain({
+  // ...chainConfig,
+  id: 88991,
+  name: 'JibchainTestnet',
+  nativeCurrency: {
+      decimals: 18,
+      name: 'tJBC',
+      symbol: 'tJBC',
+  },
+  rpcUrls: {
+      default: {
+          http: ['https://rpc.testnet.jibchain.net'],
+          webSocket: [''],
+      },
+  },
+  blockExplorers: {
+      default: { name: 'Explorer', url: 'https://exp.testnet.jibchain.net/' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xa1a858ad9041B4741e620355a3F96B3c78e70ecE',
+      blockCreated: 32848,
+    },
+  },
+});
 
 const getChain = (chainId: number) => {
   const supportedChains = [
@@ -37,7 +62,7 @@ const getChain = (chainId: number) => {
     sepolia,
     optimism,
     baseSepolia,
-    optimismSepolia,
+    optimismSepolia, JibchainTestnet
   ];
   const detectedChain = supportedChains.find((chain) => chain.id === chainId);
   if (!detectedChain) {
@@ -85,6 +110,11 @@ export const rollupChain = defineChain({
         address: import.meta.env.VITE_L1_OPTIMISM_PORTAL_ADDRESS,
       },
     },
+    l1StandardBridge: {
+      [parentChain.id]: {
+        address: import.meta.env.VITE_L1_STANDARD_BRIDGE_ADDRESS,
+      },
+    },
   },
   iconBackground: import.meta.env.VITE_L2_ICON_BACKGROUND_COLOR,
   iconUrl: import.meta.env.VITE_L2_ICON_URL,
@@ -119,11 +149,32 @@ export const token = getContract({
   client: parentClient,
 });
 
+export const newToken1 = getContract({
+  address: import.meta.env.VITE_NEW_TOKEN1_ADDRESS,
+  abi: erc20Abi,
+  client: parentClient,
+});
+
+export const newToken2 = getContract({
+  address: import.meta.env.VITE_NEW_TOKEN2_ADDRESS,
+  abi: erc20Abi,
+  client: parentClient,
+});
+
+
+
 export const optimismPortal = getContract({
   address: import.meta.env.VITE_L1_OPTIMISM_PORTAL_ADDRESS,
   abi: optimismPortalAbi,
   client: parentClient,
 });
+
+// export const l1StandardBridge = getContract({
+//   address: import.meta.env.VITE_L1_STANDARD_BRIDGE_ADDRESS,
+//   abi: l1StandardBridgeAbi,
+//   client: parentClient,
+// });
+
 
 export const config = createConfig({
   // connectors: connectors,
